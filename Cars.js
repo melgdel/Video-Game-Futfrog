5e508f4735c885bad2519df85431702db2743044
Melissa Delgado Infante <melissadelgadoinfante@Melissas-MacBook-Pro.local>
2019-03-10 22:10:12 +0100

    added two new JS files for Car functions and log function

---
+++ Cars.js
@@ -0,0 +1,217 @@
+
+
+function init_game() {
+	canvas = document.getElementById ('game');
+	if (canvas.getContext) {
+		ctx = canvas.getContext('2d');
+		rungame(ctx);
+	}
+	
+}
+
+/* initializes car positions and speeds */
+function init_cars() {
+	vars.car = new Array();
+	vars.car.spacing = 90;
+	vars.car[0] = {};
+	vars.car[0].pX = 800;
+	vars.car[0].pY = 460;
+	vars.car[0].speed = -0.8;
+	vars.car[0].len = 50;
+	vars.car[1] = {};
+	vars.car[1].pX = 0;
+	vars.car[1].pY = 428;
+	vars.car[1].speed = 1.3;
+	vars.car[1].len = 50;
+	vars.car[2] = {};
+	vars.car[2].pX = 800;
+	vars.car[2].pY = 390;
+	vars.car[2].speed = -1.1;
+	vars.car[2].len = 50;
+	vars.car[3] = {};
+	vars.car[3].pX = 0;
+	vars.car[3].pY = 355;
+	vars.car[3].speed = 1.5;
+	vars.car[3].len = 50;
+	vars.car[4] = {};
+	vars.car[4].pX = 800;
+	vars.car[4].pY = 330;
+	vars.car[4].speed = -2.5;
+	vars.car[4].len = 50;
+}
+
+// draws the game's cars */
+function drawCars() {
+	//ROW 1
+	ctx.drawImage(car1,0,0,170,116,vars.car[0].pX,vars.car[0].pY,28,24);
+
+	ctx.drawImage(sprite,10,267,28,20,vars.car[0].pX + vars.car[0].len + vars.car.spacing,
+				 vars.car[0].pY,28,20);
+	//ROW 2
+	ctx.drawImage(sprite,46,265,28,24,vars.car[1].pX,vars.car[1].pY,28,24);
+ ctx.drawImage(sprite,46,265,28,24,vars.car[1].pX - vars.car.spacing - vars.car[1].len,
+				  vars.car[1].pY,28,24);
+	//ROW 3
+	ctx.drawImage(sprite,82,264,24,26,vars.car[2].pX,vars.car[2].pY,24,26);
+  ctx.drawImage(sprite,82,264,24,26,vars.car[2].pX + vars.car[2].len + vars.car.spacing,
+				  vars.car[2].pY,24,26);
+	//ROW 4
+	ctx.drawImage(sprite,73,301,25,22,vars.car[3].pX,vars.car[3].pY,25,22);
+	ctx.drawImage(sprite,73,301,25,22,vars.car[3].pX - vars.car.spacing - vars.car[3].len,
+				  vars.car[3].pY,25,22);
+	//ROW 5
+	ctx.drawImage(sprite,106,302,46,18,vars.car[4].pX,vars.car[4].pY,46,18);
+	ctx.drawImage(sprite,106,302,46,18,vars.car[4].pX + vars.car[4].len + vars.car.spacing,
+				 vars.car[4].pY,46,18);}
+
+/* checks if the frog has been run over by a car. calls a function depending on the row
+   the frog is in */
+	 function checkDeathByCar() {
+		if ((vars.frogY +17 >= vars.car[0].pY) && (vars.frogY <= vars.car[0].pY + 20)) {
+			if (!checkCarRow1()) {
+				loseLife();
+			}
+			else return;
+		}
+		else if ((vars.frogY + 17 >= vars.car[1].pY) && (vars.frogY <= vars.car[1].pY + 24)) {
+			if (!checkCarRow2()) {
+				loseLife();
+			}
+			else return;
+		}
+		else if ((vars.frogY + 17 >= vars.car[2].pY) && (vars.frogY <= vars.car[2].pY + 26)) {
+			if (!checkCarRow3()) {
+				loseLife();
+			}
+			else return;
+		}
+		else if ((vars.frogY + 17 >= vars.car[3].pY) && (vars.frogY <= vars.car[3].pY + 22)) {
+			if (!checkCarRow4()) {
+				loseLife();
+			}
+			else return;
+		}
+		else if ((vars.frogY + 17 >= vars.car[4].pY) && (vars.frogY <= vars.car[4].pY + 18)) {
+			if (!checkCarRow5()) {
+				loseLife();
+			}
+			else return;
+		}
+		else return true;
+	
+	}
+	
+	/* checks for car collisions if the frog is in car row 1 */
+	function checkCarRow1() {
+		if ((vars.frogX + 23 >= vars.car[0].pX) && 
+			(vars.frogX <= vars.car[0].pX + 28)) {
+					return false;
+		}
+		else if ((vars.frogX + 23 >= (vars.car[0].pX + vars.car.spacing + vars.car[0].len)) && 
+				(vars.frogX <= vars.car[0].pX + vars.car.spacing + 2*vars.car[0].len)) {
+					return false;
+		}
+		else {
+					return true;
+		}
+	}
+	
+	/* checks for car collisions if the frog is in car row 2 */
+	function checkCarRow2() {
+		if ((vars.frogX <= vars.car[1].pX + 28) && 
+			(vars.frogX + 23 >= vars.car[1].pX)) {
+					return false;
+		}
+		else if ((vars.frogX <= (vars.car[1].pX - vars.car.spacing)) && 
+				(vars.frogX + 23 >= vars.car[1].pX - vars.car.spacing - 28)) {
+					return false;
+		}
+		else {
+					return true;
+		}
+	}
+	
+	/* checks for car collisions if the frog is in car row 3 */
+	function checkCarRow3() {
+		if ((vars.frogX + 23 >= vars.car[2].pX) && 
+			(vars.frogX <= vars.car[2].pX + 24)) {
+					return false;
+		}
+		else if ((vars.frogX + 23 >= (vars.car[2].pX + vars.car.spacing + vars.car[2].len)) && 
+				(vars.frogX <= vars.car[2].pX + vars.car.spacing + 2*vars.car[2].len)) {
+					return false;
+		}
+		else {
+					return true;
+		}
+	}
+	
+	/* checks for car collisions if the frog is in car row 4 */
+	function checkCarRow4() {
+		if ((vars.frogX <= vars.car[3].pX + 25) && 
+			(vars.frogX + 23 >= vars.car[3].pX)) {
+					return false;
+		}
+		else if ((vars.frogX <= (vars.car[3].pX - vars.car.spacing)) && 
+				(vars.frogX + 23 >= vars.car[3].pX - vars.car.spacing - 25)) {
+					return false;
+		}
+		else {
+					return true;
+		}
+	}
+	
+	/* checks for car collisions if the frog is in car row 5 */
+	function checkCarRow5() {
+		if ((vars.frogX + 23 >= vars.car[4].pX) && 
+			(vars.frogX <= vars.car[4].pX + 46)) {
+					return false;
+		}
+		else if ((vars.frogX + 23 >= (vars.car[4].pX + vars.car.spacing + vars.car[4].len)) && 
+				(vars.frogX <= vars.car[4].pX + vars.car.spacing + 2*vars.car[4].len)) {
+					return false;
+		}
+		else {
+					return true;
+		}
+	}
+
+	/* moves cars across the screen by adding to their position at a constant rate */
+function moveCars() {
+	//ROW 1 goes left
+	if (vars.car[0].pX < -200) {
+		vars.car[0].pX = 800;	//right edge of canvas
+	}
+	else {
+		vars.car[0].pX += vars.car[0].speed ;
+	}
+	//ROW 2 goes right
+	if (vars.car[1].pX > 530) {
+		vars.car[1].pX = -vars.car[1].len;
+	}
+	else {
+		vars.car[1].pX += vars.car[1].speed;
+	}
+	//ROW 3 goes left
+	if (vars.car[2].pX < -240) {
+		vars.car[2].pX = 800;	//right edge of canvas
+	}
+	else {
+		vars.car[2].pX += vars.car[2].speed;
+	}
+	//ROW 4 goes right
+	if (vars.car[3].pX > 520) {
+		vars.car[3].pX = -vars.car[3].len;
+	}
+	else {
+		vars.car[3].pX += vars.car[3].speed;
+	}
+	//ROW 5 goes left
+	if (vars.car[4].pX < -240) {
+		vars.car[4].pX = 800;
+	}
+	else {
+		vars.car[4].pX += vars.car[4].speed;
+	}
+}
+
---
+++ Logs.js
@@ -0,0 +1,258 @@
+/* gets the page canvas, checks for context, and runs the game */
+function init_game() {
+	canvas = document.getElementById ('game');
+	if (canvas.getContext) {
+		ctx = canvas.getContext('2d');
+		rungame(ctx);
+	}
+	
+}
+
+// initializes log positions and speeds 
+function init_logs() {
+	vars.log = new Array();
+	vars.log[0] = {};
+	vars.log[0].pX = 180;		//1 goes left
+	vars.log[0].pY = 252;
+	vars.log[0].loglen = 85;
+	vars.log[0].speed = -0.8;
+	vars.log[1] = {};
+	vars.log[1].loglen = 178;
+	vars.log[1].pX = 250;		//ROW 2 goes right
+	vars.log[1].pY = 218;
+	vars.log[1].speed = 0.9;
+	vars.log[2] = {};
+	vars.log[2].loglen = 117;
+	vars.log[2].pX = 300;		//ROW 3 goes left
+	vars.log[2].pY = 184;
+	vars.log[2].speed = -1.0;
+	vars.log[3] = {};
+	vars.log[3].loglen = 85;
+	vars.log[3].pX = 170;		//ROW 4 goes right
+	vars.log[3].pY = 150;
+	vars.log[3].speed = 1.3;
+	vars.log[4] = {};
+	vars.log[4].loglen = 178;
+	vars.log[4].pX = 130;		//ROW 5 goes right
+	vars.log[4].pY = 116;
+	vars.log[4].speed = 1.5;
+	vars.logrowspaceS = 250;
+	vars.logrowspaceM = 300;
+	vars.logrowspaceL = 350;
+}
+
+//draws the game's logs 
+function drawLogs() {
+	//ROW 1 Small
+	ctx.drawImage(sprite,7,230,85,21,vars.log[0].pX,vars.log[0].pY,85,21);
+	ctx.drawImage(sprite,7,230,85,21,vars.log[0].pX + vars.logrowspaceS,vars.log[0].pY,85,21);
+	ctx.drawImage(sprite,7,230,85,21,vars.log[0].pX + 2*vars.logrowspaceS,vars.log[0].pY,85,21);
+	//ROW 2 Large
+	ctx.drawImage(sprite,7,165,178,22,vars.log[1].pX,vars.log[1].pY,178,22);
+	ctx.drawImage(sprite,7,165,178,22,vars.log[1].pX - vars.logrowspaceL,vars.log[1].pY,178,22);
+	//ROW 3 Medium
+	ctx.drawImage(sprite,7,198,117,21,vars.log[2].pX,vars.log[2].pY,117,21);
+	ctx.drawImage(sprite,7,198,117,21,vars.log[2].pX + vars.logrowspaceM,vars.log[2].pY,117,21);
+	//ROW 4 Small
+	ctx.drawImage(sprite,7,230,85,21,vars.log[3].pX,vars.log[3].pY,85,21);
+	ctx.drawImage(sprite,7,230,85,21,vars.log[3].pX - vars.logrowspaceS,vars.log[3].pY,85,21);
+	ctx.drawImage(sprite,7,230,85,21,vars.log[3].pX - 2*vars.logrowspaceS,vars.log[3].pY,85,21);
+	//ROW 5 Large
+	ctx.drawImage(sprite,7,165,178,22,vars.log[4].pX,vars.log[4].pY,178,22);
+	ctx.drawImage(sprite,7,165,178,22,vars.log[4].pX - vars.logrowspaceL,vars.log[4].pY,178,22);
+}
+
+// moves logs across the screen by adding to their position at a constant rate 
+function moveLogs() {
+	//ROW 1 goes left
+	if (vars.log[0].pX < -365) {
+		vars.log[0].pX =800;	//right edge of canvas
+	}
+	else {
+		vars.log[0].pX += vars.log[0].speed ;
+	}
+	//ROW 2 goes right
+	if (vars.log[1].pX > 680) {
+		vars.log[1].pX = -vars.log[4].loglen;
+	}
+	else {
+		vars.log[1].pX += vars.log[1].speed;
+	}
+	//ROW 3 goes left
+	if (vars.log[2].pX < -280) {
+		vars.log[2].pX = 800;	//right edge of canvas
+	}
+	else {
+		vars.log[2].pX += vars.log[2].speed;
+	}
+	//ROW 4 goes right
+	if (vars.log[3].pX > 665) {
+		vars.log[3].pX = -vars.log[3].loglen;
+	}
+	else {
+		vars.log[3].pX += vars.log[3].speed;
+	}
+	//ROW 5 goes right
+	if (vars.log[4].pX > 680) {
+		vars.log[4].pX = -vars.log[4].loglen;
+	}
+	else {
+		vars.log[4].pX += vars.log[4].speed;
+	}
+}
+
+//checks if the frog is on a log. If so, the frog moves with the log 
+function frogOnLog() {
+	if (vars.frogDead == true) {
+		return;
+	}
+	if ((vars.frogY - 4 >= vars.log[0].pY) && (vars.frogY <= vars.log[0].pY + 25)) {
+		if (checkWaterRow1()) {
+			vars.frogX += vars.log[0].speed;
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[1].pY) && (vars.frogY <= vars.log[1].pY + 25)) {
+		if (checkWaterRow2()) {
+			vars.frogX += vars.log[1].speed;
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[2].pY) && (vars.frogY <= vars.log[2].pY + 25)) {
+		if (checkWaterRow3()) {
+			vars.frogX += vars.log[2].speed;
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[3].pY) && (vars.frogY <= vars.log[3].pY + 25)) {
+		if (checkWaterRow4()) {
+			vars.frogX += vars.log[3].speed;
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[4].pY) && (vars.frogY <= vars.log[4].pY + 25)) {
+		if (checkWaterRow5()) {
+			vars.frogX += vars.log[4].speed;
+		}
+		else return;
+	}
+}
+
+//checks if the frog has drowned. calls a function depending on the row
+  // the frog is in 
+function checkDeathByWater() {
+	if ((vars.frogY - 4 >= vars.log[0].pY) && (vars.frogY <= vars.log[0].pY + 25)) {
+		if (!checkWaterRow1()) {
+			loseLife();
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[1].pY) && (vars.frogY <= vars.log[1].pY + 25)) {
+		if (!checkWaterRow2()) {
+			loseLife();
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[2].pY) && (vars.frogY <= vars.log[2].pY + 25)) {
+		if (!checkWaterRow3()) {
+			loseLife();
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[3].pY) && (vars.frogY <= vars.log[3].pY + 25)) {
+		if (!checkWaterRow4()) {
+			loseLife();
+		}
+		else return;
+	}
+	else if ((vars.frogY - 4 >= vars.log[4].pY) && (vars.frogY <= vars.log[4].pY + 25)) {
+		if (!checkWaterRow5()) {
+			loseLife();
+		}
+		else return;
+	}
+	else return true;
+}
+
+//checks for drowning if the frog is in water row 1 
+function checkWaterRow1() {
+	if ((vars.frogX >= vars.log[0].pX - 15) && 
+		(vars.frogX <= vars.log[0].pX + vars.log[0].loglen - 20)) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[0].pX + vars.logrowspaceS - 15)) && 
+			(vars.frogX <= (vars.log[0].pX + vars.logrowspaceS + vars.log[0].loglen - 20))) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[0].pX + 2*vars.logrowspaceS - 15)) && 
+			 (vars.frogX <= (vars.log[0].pX + 2*vars.logrowspaceS + vars.log[0].loglen - 20))) {
+				return true;
+	}	
+	else {
+				return false;
+	}
+}
+
+//checks for drowning if the frog is in water row 2 
+function checkWaterRow2() {	
+	if ((vars.frogX >= vars.log[1].pX - 15) && 
+		(vars.frogX <= vars.log[1].pX + vars.log[1].loglen - 20)) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[1].pX - vars.logrowspaceL - 15)) && 
+			 (vars.frogX <= (vars.log[1].pX - vars.logrowspaceL + vars.log[1].loglen - 20))) {
+				return true;
+	}
+	else {
+				return false;
+	}
+}
+
+//checks for drowning if the frog is in water row 3 
+function checkWaterRow3() {
+	if ((vars.frogX >= vars.log[2].pX - 10) && 
+		(vars.frogX <= vars.log[2].pX + vars.log[2].loglen - 15)) {
+			return true;
+	}
+	else if ((vars.frogX >= (vars.log[2].pX + vars.logrowspaceM - 15)) && 
+			 (vars.frogX <= (vars.log[2].pX + vars.logrowspaceM + vars.log[2].loglen - 20))) {
+			return true;
+	}
+	else {
+			return false;	
+	}
+}
+
+//checks for drowning if the frog is in water row 4 
+function checkWaterRow4() {
+	if ((vars.frogX >= vars.log[3].pX - 15) && 
+		(vars.frogX <= vars.log[3].pX + vars.log[3].loglen - 20)) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[3].pX - vars.logrowspaceS - 15)) && 
+			 (vars.frogX <= (vars.log[3].pX - vars.logrowspaceS + vars.log[3].loglen - 25))) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[3].pX - 2*vars.logrowspaceS - 15)) && 
+			 (vars.frogX <= (vars.log[3].pX - 2*vars.logrowspaceS + vars.log[3].loglen - 20))) {
+				return true;
+	}
+	else {
+				return false;
+	}
+}
+
+//checks for drowning if the frog is in water row 5 
+function checkWaterRow5() {
+	if ((vars.frogX >= vars.log[4].pX - 15) && 
+		(vars.frogX <= vars.log[4].pX + vars.log[4].loglen - 20)) {
+				return true;
+	}
+	else if ((vars.frogX >= (vars.log[4].pX - vars.logrowspaceL - 15)) && 
+			 (vars.frogX <= (vars.log[4].pX - vars.logrowspaceL + vars.log[4].loglen - 20))) {
+				return true;
+	}
+	else {
+				return false;
+	}
+}
